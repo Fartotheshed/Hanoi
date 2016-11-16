16d9e79c76492c133babca8dae296c424e6477f1
Syed Aman <sna274@nyu.edu>
2016-11-16 15:11:30 -0500

    tidy up

--- Towers.cpp
+++ Towers.cpp
@@ -12,30 +12,31 @@ int main(int argc, char *argv[]){
         {"end", required_argument, 0, 'e'},
         {"num-disks", required_argument, 0, 'n'},
         {0, 0, 0, 0}
+    };
+
+    char c;
+    int opt_index = 0;
+    short n = 0, start = 0, end = 0;
+
+    while((c = getopt_long(argc, argv, "s:e:n:", long_opts, &opt_index)) != -1) {
+        switch (c) {
+            case 's':
+            start = atoi(optarg);
+            break;
+            case 'e':
+            end = atoi(optarg);
+            break;
+            case 'n':
+            n = atoi(optarg);
+            break;
+            case '?':
+
+            return 1;
+        }
+    }
+
+    return 0;
 };
-
-char c;
-int opt_index = 0;
-short n = 0, start = 0, end = 0;
-
-while((c = getopt_long(argc, argv, "s:e:n:", long_opts, &opt_index)) != -1) {
-    switch (c) {
-    case 's':
-    start = atoi(optarg);
-    break;
-    case 'e':
-    end = atoi(optarg);
-    break;
-    case 'n':
-    n = atoi(optarg);
-    break;
-    case '?':
-
-    return 1;
-}
-}
-   return 0;
-}
 
 void towers(int n, int start, int middle, int end)
 {
