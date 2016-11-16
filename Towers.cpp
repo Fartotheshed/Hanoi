7444f5414a9fb532f14759df177e2e7d2c16d56a
Syed Aman <sna274@nyu.edu>
2016-11-16 14:42:16 -0500

    fix syntax errors

--- Towers.cpp
+++ Towers.cpp
@@ -6,48 +6,46 @@ using std::endl;
 #include <cstdlib>
 using namespace std;
 
-int main(int argc, char *argv[])     {
-
-static struct option long_opts[] = {
-    {"start",           required_argument, 0, 's'},
-    {"end",             required_argument, 0, 'e'},
-    {"num-disks"        required_argument, 0, 'n'},
-    {0, 0, 0, 0}
+int main(int argc, char *argv[]){
+    static struct option long_opts[] = {
+        {"start", required_argument, 0, 's'},
+        {"end", required_argument, 0, 'e'},
+        {"num-disks", required_argument, 0, 'n'},
+        {0, 0, 0, 0}
 };
 
 char c;
 int opt_index = 0;
 short n = 0, start = 0, end = 0;
-while ((c = getopt_long(argc, argv, "s:e:n:", long_opts, &opt_index)) != -1) {
+
+while((c = getopt_long(argc, argv, "s:e:n:", long_opts, &opt_index)) != -1) {
     switch (c) {
-      case 's':
-       start = atoi(optarg);
-       break;
-      case 'e':
-       end = atoi(optarg);
-       break;
-      case 'n':
-       n = atoi(optarg);
-       break;
-      case '?':
+    case 's':
+    start = atoi(optarg);
+    break;
+    case 'e':
+    end = atoi(optarg);
+    break;
+    case 'n':
+    n = atoi(optarg);
+    break;
+    case '?':
 
-       return 1;
-   }
-  }       
-
-
+    return 1;
+}
+}
    return 0;
 }
 
-
-
-void towers(int n, int start, int middle int end)
+void towers(int n, int start, int middle, int end)
 {
-  if(n == 1) {
-      cout << "Move disk" << n << "from" << start << "to" << end << endl;
-  }
-  else() {
-      towers(n-1,start,end,middle);
-      towers(1,start,middle,end);
-      towers(n-1,middle,beginning,end);
-  }
+    if(n == 1){
+        cout << "Move disk" << n << "from" << start << "to" << end << endl;
+    }
+
+    else{
+        towers(n-1,start,end,middle);
+        towers(1,start,middle,end);
+        towers(n-1,middle,beginning,end);
+    }
+};
